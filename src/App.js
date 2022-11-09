c502b412657bb221b5ea4760a2b9cb5850982b16
nanun94 <youngje3431@gmail.com>
2022-11-09 21:54:26 +0900

    20221109 state 참조형 다루는법

--- src/App.js
+++ src/App.js
@@ -19,13 +19,26 @@ function App() {
    * 1번째 값을 변경할때에는 반드시 setter 함수로 변경해주어야 한다.
    */
   const [num, setNum] = useState(1);
+  //점수의 합계를 구할 states
+  const [sum, setSum] = useState(0);
+  //참조형 state
+  //참조형 (배열) 은 변수가 주소를 가지고 있기 때문에 주소값이 같다면 state 가 변경되었다고 판단하지 않는다.
+  //따라서 참조형으로 state를 사용할때에는 화면이 바뀌기 위해서는 반드시 새로운 값으로 할당해주어야 한다.
+  const [gameHistory, setGameHistory] = useState([]);
+
   const handleRollClick = () => {
     const nextNum = random(6);
     setNum(nextNum);
+    setSum(sum + nextNum);
+    //gameHistory.push(nextNum);
+    setGameHistory([...gameHistory, nextNum]);
   };
   const handleClearClick = () => {
     setNum(1);
+    setSum(0);
+    setGameHistory([]);
   };
+
   //컴포넌트에 props 값을 세팅
   return (
     <div>
@@ -33,7 +46,13 @@ function App() {
         <Button onClick={handleRollClick}>던지기</Button>
         <Button onClick={handleClearClick}>처음부터</Button>
       </div>
-      <Dice color="blue" num={num} />
+      <div>
+        <h2>나</h2>
+        <Dice color="blue" num={num} />
+        <h2>총점</h2>
+        <p>{sum}</p>
+        <p>{gameHistory.join(', ')}</p>
+      </div>
     </div>
   );
 }
